c1ca0ccf0b8013ba148a71b8e299dcc7693f7135
wongxy <xiyao.wong@foxmail.com>
2020-05-26 22:23:52 +0800

    update

--- MeiZiTu.lua
+++ MeiZiTu.lua
@@ -8,25 +8,26 @@ function ReceiveFriendMsg(CurrentQQ, data)
 end
 
 function ReceiveGroupMsg(CurrentQQ, data)
-    if string.find(data.Content, '妹子') 
-        or string.find(data.Content, '色图') 
-        or string.find(data.Content, '美女') then
-       api.Api_SendMsg(
-           CurrentQQ,
-            {
-              toUser = data.FromGroupId,
-              sendToType = 2,
-              sendMsgType = "PicMsg",
-              content = "",
-              groupid = 0,
-              atUser = 0,
-              picUrl = "http://127.0.0.1:8080/mzt/?both=1",
-              picBase64Buf = "",
-              fileMd5 = ""
-            }
-       )
+    if data.FromGroupId ~= 818316898 then
+        if string.find(data.Content, '妹子') 
+            or string.find(data.Content, '色图') 
+            or string.find(data.Content, '美女') then
+           api.Api_SendMsg(
+               CurrentQQ,
+                {
+                  toUser = data.FromGroupId,
+                  sendToType = 2,
+                  sendMsgType = "PicMsg",
+                  content = "",
+                  groupid = 0,
+                  atUser = 0,
+                  picUrl = "http://127.0.0.1:8080/mzt/?both=1",
+                  picBase64Buf = "",
+                  fileMd5 = ""
+                }
+           )
+        end
     end
-
     return 1
 end
 
