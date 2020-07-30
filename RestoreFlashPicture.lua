4332700f0c19134cdb394434b97e063af7c1da1c
wongxy <xiyao.wong@foxmail.com>
2020-07-30 21:16:39 +0800

    闪照还原

---
+++ RestoreFlashPicture.lua
@@ -0,0 +1,35 @@
+-- 发送闪照原图
+local api = require("coreApi")
+local http = require("http")
+local json = require("json")
+
+function ReceiveFriendMsg(CurrentQQ, data)
+    return 1
+end
+
+function ReceiveGroupMsg(CurrentQQ, data)
+    if data.FromUserId == tonumber(CurrentQQ) then
+        return 1
+    end
+    if data.MsgType == 'PicMsg' and data.Content:find('请使用新版手机QQ查看闪照') then
+        api.Api_SendMsg(
+            CurrentQQ,
+            {
+                toUser = data.FromGroupId,
+                sendToType = 2,
+                sendMsgType = "PicMsg",
+                content = "你竟然发闪照[表情176]",
+                groupid = 0,
+                atUser = 0,
+                picUrl = json.decode(data.Content).Url,
+                picBase64Buf = "",
+                fileMd5 = ""
+            }
+       )
+    end
+    return 1
+end
+
+function ReceiveEvents(CurrentQQ, data, extData)
+    return 1
+end
