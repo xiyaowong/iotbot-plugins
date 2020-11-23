51fba1b8d6e69d5246ecce60bc9a9769efdb6d8a
wongxy <xiyao.wong@foxmail.com>
2020-11-23 18:58:36 +0800

    Add watch the world...

---
+++ watch_the_world_in_60_seconds.lua
@@ -0,0 +1,23 @@
+-- 60s看世界 `看世界`
+local Api = require("coreApi")
+
+function ReceiveGroupMsg(CurrentQQ, data) 
+    if data.FromUserId == tonumber(CurrentQQ) then
+        return 1
+    end
+    if data.Content == '看世界' then
+        Api.Api_SendMsg(
+            CurrentQQ,
+            {
+                toUser = data.FromGroupId,
+                sendToType = 2,
+                sendMsgType = "PicMsg",
+                picUrl = 'http://yanxi520.xyz/api/60s/min.php?address=1&name=%E5%B0%8F%E5%8A%A9%E6%89%8B',
+            }
+        )
+    end
+    return 1 
+end
+
+function ReceiveFriendMsg(CurrentQQ, data) return 1 end
+function ReceiveEvents(CurrentQQ, data, extData) return 1 end
